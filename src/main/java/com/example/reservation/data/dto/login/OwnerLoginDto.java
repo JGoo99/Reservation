bb0566673c07e81d52bf6d6d5cb54d47a19a70a5
JGoo99 <zzgg3040@naver.com>
2024-01-02 13:27:47 +0900

    style: 홈화면 구성 및 회원관리 프론트 수정

---
+++ src/main/java/com/example/reservation/data/dto/login/OwnerLoginDto.java
@@ -0,0 +1,22 @@
+package com.example.reservation.data.dto.login;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+public class OwnerLoginDto {
+
+  @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
+  @NotBlank
+  private String email;
+
+  @NotBlank
+  private String password;
+}
