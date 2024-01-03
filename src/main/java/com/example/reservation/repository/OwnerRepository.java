2879879d41a90c74d17c2fe7708377b9a642700d
JGoo99 <zzgg3040@naver.com>
2024-01-03 14:31:32 +0900

    refactor:

--- src/main/java/com/example/reservation/repository/OwnerRepository.java
+++ src/main/java/com/example/reservation/repository/OwnerRepository.java
@@ -1,8 +1,10 @@
 package com.example.reservation.repository;
 
 import com.example.reservation.data.entity.Owner;
+import com.example.reservation.data.entity.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
